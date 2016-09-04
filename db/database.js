6317d8949d17de6953ffad0df97b12462c43ed04
Simeon Lee <simeonhlee@gmail.com>
2016-09-04 15:32:31 -0700

    Add middleware including Multer and fs that will allow app to save images

--- db/database.js
+++ db/database.js
@@ -1,5 +1,10 @@
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
+var fs = require('fs');
+var multer = require('multer');
+
+// Import 'Memly' mongoose model
+var Memly = require('./memlys/memlyModel');
 
 var mongooseUri =
   process.env.MONGODB_URI ||
@@ -7,17 +12,29 @@ var mongooseUri =
 
 mongoose.connect(mongooseUri);
 
-// We will not have db.once('open', cb) and just have saving
-// and finding operations to database buffer
+// Call the below on app to enable it to upload images to database
+module.exports = function(app) {
 
-// var db = mongoose.connection;
+	// Use 'Multer' middleware to take a photo and put it in a folder
+	// called 'uploads' so we can easily access it later
+	// Multer is middleware for handling 'multipart/form-data',
+	// which is primarily used for uploading files
+	// https://github.com/expressjs/multer
+	app.use(multer({ dest: './uploads/',
+		rename: function(fieldname, filename) {
+			return filename;
+		}
+	}));
 
-// db.on('error', console.error.bind(console, 'connection error:'));
+	app.post('/api/photo', function(req, res) {
+		// Create instance of Memly
+		var newMemly = new Memly();
+		// Memly image.data key will have value of image data
+		newMemly.image.data = fs.readFileSync(req.files.userPhoto.path);
+		// Memly image.contentType specifies that we have a 'png' file
+		newMemly.image.contentType = 'image/png';
+		// Save Memly to database
+		newMemly.save();
+	})
 
-// db.once('open', function() {
-//   console.log('Database is connected');
-
-
-  
-// });
-
+};
