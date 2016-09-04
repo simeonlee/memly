eab3e40608d73e923601fa933dc611844dce440d
Simeon Lee <simeonhlee@gmail.com>
2016-09-04 15:32:09 -0700

    Add image data property keys to Memly model schema

--- db/memlys/memlyModel.js
+++ db/memlys/memlyModel.js
@@ -5,8 +5,11 @@ var memlySchema = new Schema({
   userId: String,
   title: String,
   mediaUrl: String,
-  visits: Number
-  // TODO: what else should describe our memly?
+  visits: Number,
+  image: {
+  	data: Buffer, // allows us to store our image as data in the form of arrays
+  	contentType: String
+  }
 });
 
 module.exports = mongoose.model('Memly', memlySchema);
