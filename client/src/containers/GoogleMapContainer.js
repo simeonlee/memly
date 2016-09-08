053adff75f15ce7e9a02d1862684e11c6a0c1895
Simeon Lee <simeonhlee@gmail.com>
2016-09-08 16:25:54 -0700

    Implement working AWS S3 Cloud Storage functionality for media

--- client/src/containers/GoogleMapContainer.js
+++ client/src/containers/GoogleMapContainer.js
@@ -34,68 +34,70 @@ class GoogleMapContainer extends Component {
         lng: -122.4089664
       },
       //memlys will be grabbed from db based on user location. (HTTP request in componentWillMount method)
-      memlys: [{
-        location: {
-          lat: 0,
-          lng: 0,
+      memlys: [
+        {
+          location: {
+            lat: 37.7836966,
+            lng: -122.4089664
+          },
+          key: 'Hack Reactor',
+          defaultAnimation: 2,
+          showInfo: false,
+          media: {
+            data: "../../styles/hackreactor.jpg"
+          }
         },
-        key: 'currentPosition',
-        defaultAnimation: 2,
-        showInfo: false,
-        photo: null
-      },
-      {
-        location: {
-          lat: 37.7836966,
-          lng: -122.4089664
+        {
+          location: {
+            lat: 51.507351,
+            lng: -0.125758
+          },
+          username: "Michael Wong",
+          userAvatar: "../../styles/userAvatar.jpg",
+          key: 'timestamp1',
+          defaultAnimation: 2,
+          showInfo: false,
+          media: {
+            data: "../../styles/shutterstock_276995975.jpg"
+          }
         },
-        key: 'Hack Reactor',
-        defaultAnimation: 2,
-        showInfo: false,
-        photo: "../../styles/hackreactor.jpg"
-      },
-      {
-        location: {
-          lat: 51.507351,
-          lng: -0.125758
+        {
+          location: {
+            lat: 51.507351,
+            lng: -0.12958
+          },
+          key: 'timestamp2',
+          defaultAnimation: 2,
+          showInfo: false,
+          media: {
+            data: "../../styles/M9071-PARENT-2.jpg"
+          }
         },
-        username: "Michael Wong",
-        userAvatar: "../../styles/userAvatar.jpg",
-        key: 'timestamp1',
-        defaultAnimation: 2,
-        showInfo: false,
-        photo: "../../styles/shutterstock_276995975.jpg"
-      },
-      {
-        location: {
-          lat: 51.507351,
-          lng: -0.12958
+        {
+          location: {
+            lat: 51.509351,
+            lng: -0.12958
+          },
+          key: 'timestamp3',
+          defaultAnimation: 2,
+          showInfo: false,
+          media: {
+            data: "../../styles/15759420184_f34af1b4a8.jpg"
+          }
         },
-        key: 'timestamp2',
-        defaultAnimation: 2,
-        showInfo: false,
-        photo: "../../styles/M9071-PARENT-2.jpg"
-      },
-      {
-        location: {
-          lat: 51.509351,
-          lng: -0.12958
-        },
-        key: 'timestamp3',
-        defaultAnimation: 2,
-        showInfo: false,
-        photo: "../../styles/15759420184_f34af1b4a8.jpg"
-      },
-      {
-        location: {
-          lat: 51.506351,
-          lng: -0.12958
-        },
-        key: 'timestamp4',
-        defaultAnimation: 2,
-        showInfo: false,
-        photo: "../../styles/londonstreet.jpeg"
-      }]
+        {
+          location: {
+            lat: 51.506351,
+            lng: -0.12958
+          },
+          key: 'timestamp4',
+          defaultAnimation: 2,
+          showInfo: false,
+          media: {
+            data: "../../styles/londonstreet.jpeg"
+          }
+        }
+      ]
     }
 
     this.geolocate();
@@ -162,6 +164,10 @@ class GoogleMapContainer extends Component {
         .then((response) => {
           // 'response.data' is an array of memlys to be displayed
           console.log(response.data);
+
+          response.data.forEach(function(memly) {
+            memly.media.data = memly.media.data.data.toString('base64');
+          });
 
           let { memlys } = this.state;
           memlys = update(memlys, { $push: response.data } );
--- client/src/containers/ImageUploadContainer.js
+++ client/src/containers/ImageUploadContainer.js
@@ -110,6 +110,3 @@ export default class ImageUploadContainer extends React.Component {
 		)
 	}
 }
-
-
-// handleSubmit={this.handleSubmit.bind(this)}
