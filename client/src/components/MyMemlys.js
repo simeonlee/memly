885b528e68d52d15d53f838fd111efe1bf46b71e
Robert Chao <robertkchao@gmail.com>
2016-09-09 23:08:01 -0700

    Refactors styling for MyMemlys and LikedMemlys

--- client/src/components/MyMemlys.js
+++ client/src/components/MyMemlys.js
@@ -4,12 +4,10 @@ import { Router, Route, hashHistory, IndexRoute, Link } from 'react-router'
 
 const MyMemlys = (props) => {
   return(
-    <div>
     <div className = "oneMemly">
     <img className = 'memlyPhoto' src= {props.item.url}/>
     <div className = "memlyInfo">
     <span>taken from: {props.item.location}</span>
-    </div>
     </div>
     </div>
     )
